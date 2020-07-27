1a593bb2cf6b4ad65823d012b1f96773a7fc92d6
Zeeshaan Noor <zeeshaannoorulislam@gmail.com>
2020-07-27 16:49:23 +0530

    String Practice Update

--- string.py
+++ string.py
@@ -25,4 +25,25 @@ greetings6 = 'My boss yelled at me \n\"GET BACK TO' + job       # Here we learn 
 print(greetings5)                                               # Another note... ONLY STRINGS CAN BE CONCATINATED.(i.e not numbers,etc)
 
 greetings7 = 'My boss yelled at me \n\"GET BACK TO WORK\"\n'
-print(greetings7.upper())                                       # Here UPPER indicates that the whole string is not CAPITALIZED
+print(greetings7.upper())                                       # Here UPPER indicates that the whole string is not CAPITALIZED.
+
+greetings8 = 'My boss yelled at me \n\"GET BACK TO WORK\"\n'    # First letter capital of all
+print(greetings8.title())                                            
+
+greetings9 = 'My boss yelled at me \n\"GET BACK TO WORK\"\n'    # The case gets swapped upper to lower and vice versa
+print(greetings9.swapcase())  
+
+greetings10 = 'My boss yelled at me \n\"GET BACK TO WORK\"\n'   # Returns the length of the string.
+print(len(greetings10))
+
+greetings11 = 'My boss yelled at me \n\"GET BACK TO WORK\"\n'   # Returns the exact character in string. PS:- it starts from 0th character
+print(greetings11[5])
+
+greetings12 = 'My boss yelled at me \n\"GET BACK TO WORK\"\n'   # Returns the range of characters lying between the mentioned range.
+print(greetings12[3:7])
+
+greetings13 = 'My boss yelled at me \"GET BACK TO WORK\"\n'   # Splits all the words with a space/gap.
+print(greetings13.split(" "))
+
+greetings13 = 'My boss yelled at me \"GET BACK TO WORK\"\n'   # Splits all the words with a space/gap to the specified allocations.
+print(greetings13.split(" ")[1:6])
