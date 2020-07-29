139537389b01549b33b81ff440be67a5b2cb2580
Zeeshaan Noor <zeeshaannoorulislam@gmail.com>
2020-07-30 01:48:13 +0530

    Assignmnet operator Tests

---
+++ assignment_operator.py
@@ -0,0 +1,33 @@
+import os
+os.system("clear")
+
+##########################
+# Assignment Operators
+# =
+# +=
+# -=
+# *=
+# /=
+# **= (EXPONENT)
+# %=
+#######################3##
+
+num = 41
+# num = num +1
+num += 2									# Adding 2 and assign it to num.
+print(num + 2 )
+
+num -= 3
+print(num)
+
+num *= 2
+print(num)
+
+num /= 8									# Rreturns float value automatically					
+print(num)
+
+num **= 2
+print(num)
+
+num %= 100 
+print(num)
