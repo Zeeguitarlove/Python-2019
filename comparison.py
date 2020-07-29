c016cc9d1ec749ffc35cea6abff51578d8ec91a6
Zeeshaan Noor <zeeshaannoorulislam@gmail.com>
2020-07-30 02:03:31 +0530

    Comparison

---
+++ comparison.py
@@ -0,0 +1,28 @@
+import os
+os.system("clear")
+
+##################################
+# Comparison Operators(gives true or false output)
+
+# == (comparing basically asking is thos equal to that)
+# !=
+# >
+# <
+# >=
+# <=
+##################################
+
+
+print(10 == 10)								# compares 10 with 10 to check if its equal/same.
+
+print(10 != 10)	
+
+print(10 >= 10)
+
+print(10 > 10)	
+
+print("zee" == "zee")
+
+print("zee" == "Zee")
+
+print([1,2,3,4,5] == [1,2,3,4,5])
