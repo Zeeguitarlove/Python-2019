2ce812206dede72a8bf1e257e67ee41a52a6f971
Zeeshaan Noor <zeeshaannoorulislam@gmail.com>
2020-07-31 02:58:42 +0530

    While Loops

---
+++ while.py
@@ -0,0 +1,18 @@
+import os
+os.system('clear')
+
+############################
+# While Loop
+############################
+
+counter = 0
+
+while(counter <= 10):
+	print("The Counter Is:" + str(counter))
+	print("\n")
+	counter += 1
+
+count = 0
+while(count < 5):
+	print("-------->" + str(count))
+	count += 1
