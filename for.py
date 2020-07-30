a639e79d6002217ff9925ee5f80db8ab1120d6c0
Zeeshaan Noor <zeeshaannoorulislam@gmail.com>
2020-07-31 03:20:43 +0530

    While Loop

---
+++ for.py
@@ -0,0 +1,15 @@
+import os
+os.system('clear')
+
+############################
+# For Loops
+############################
+
+name = "zeeshaan"
+
+for x in name:
+	print(x)
+
+name = ["John","Mary","Zee"]
+for lists in name:
+	print(lists)
