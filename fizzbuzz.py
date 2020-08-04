557a515afec9e22bdbf7821bd1c18a76c8ee7a17
Zeeshaan Noor <zeeshaannoorulislam@gmail.com>
2020-08-05 03:02:46 +0530

    FizzBuzz with Number format i.e 1,000,000

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -9,17 +9,33 @@ os.system("clear")
 ####################################
 
 num = 1
-while (num <= 100):
+count_fizz = 0
+count_buzz = 0
+count_fizzbuzz = 0
+count = 0
+
+while (num <= 1000000):
 	if(num % 3 ==0) and (num % 5 == 0):
 		print("\nFizzBuzz on ",num)
+		count_fizzbuzz += 1
 
 	elif(num % 5 == 0):
 		print("\nBuzz on ",num)
+		count_buzz += 1
 
 	elif(num % 3 == 0):
 		print("\nFizz on ",num)
-
+		count_fizz += 1
 	else:
-		print("\nNeither Fizz Nor Buzz on " + str(num))
+		print("\nNeither Fizz Nor Buzz on " + str("{:,}".format(num)))
+		count += 1
 
 	num += 1
+
+print("\nTotal number of FizzBuzz are " + str("{:,}".format(count_fizzbuzz)))
+
+print("\nTotal number of Fizz are " + str("{:,}".format(count_fizz)))
+
+print("\nTotal number of Buzz are " + str("{:,}".format(count_buzz)))
+
+print("\nTotal number of invaluable numbers are " + str("{:,}".format(count)))
